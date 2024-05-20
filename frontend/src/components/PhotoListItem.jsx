2b922b27eed00f381b4db93d61aab655c7f6f34c
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-20 14:08:41 -0400

    Modify className of li to photo-list_item block

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,17 +6,15 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
 
   return (
-    <li className="photo-list" key={props.listItem.id}>
-      <div className="photo-list__item">
-        <img className="photo-list__image" src={props.listItem.imageSource}></img>
-        <div className="photo-list__user-details">
-          <img className="photo-list__user-profile" src={props.listItem.profile}></img>
-          <div className="photo-list__user-info">
-            <span>{props.listItem.username}</span>
-            <div className="photo-list__user-location">
-              <span>{props.listItem.location.city}</span>
-              <span>{props.listItem.location.country}</span>
-            </div>
+    <li className="photo-list__item" key={props.listItem.id}>
+      <img className="photo-list__image" src={props.listItem.imageSource}></img>
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.listItem.profile}></img>
+        <div className="photo-list__user-info">
+          <span>{props.listItem.username}</span>
+          <div className="photo-list__user-location">
+            <span>{props.listItem.location.city}</span>
+            <span>{props.listItem.location.country}</span>
           </div>
         </div>
       </div>
