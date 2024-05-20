6af71a2568b9de3ba0e0041531ae7cfc5e06d59f
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-20 19:39:04 -0400

    Create component to display all topics from sample data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
 const sampleDataForTopicList = [
   {
@@ -21,9 +22,17 @@ const sampleDataForTopicList = [
 ];
 
 const TopicList = () => {
+  
+  const parsedTopic = sampleDataForTopicList.map((topic) =>
+    <TopicListItem
+    key={topic.id}
+    label={topic.title}
+    />
+    )
+
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {parsedTopic}
     </div>
   );
 };
