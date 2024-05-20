0e9d62f702303134adc58453dda84760ec3f4456
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-20 15:43:07 -0400

    Add functionality to FavIcon based on user clicks

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,12 +3,23 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+
+  //set inital state to unclicked icon
+  const [like, setLike] = useState(false);
+  
+  //set state to the opposite of the state like is currently at when clicked
+  const handleClick = () => {
+    setLike(!like);
+    console.log("the photo is liked!");
+  };
+
+  //change icon style if fav state= true
+  const favIconClass = like ? "photo-list__fav-icon-svg" : "photo-list__fav-icon";
+
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
-      </div>
+    <div className={favIconClass} onClick={handleClick}>
+      <FavIcon />
     </div>
   );
 }
