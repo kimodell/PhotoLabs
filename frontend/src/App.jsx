0ea3188ffc7ab70116bd47a9a1d4665374a04e25
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-20 16:33:44 -0400

    Remove hardcoded sample data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,16 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
-
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-//crate an array to fill with photo items
-const photos = new Array(3)
-
+ // import PhotoListItem from './components/PhotoListItem';
+ import PhotoList from './components/PhotoList';
+ import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {/*fill photos array with undefined values, map over each element, ignoring the value and using index as key for each element to be rendered*/} 
-      {photos.fill().map((_, index) =>
-      <PhotoListItem key={index} listItem={sampleDataForPhotoListItem} />
-      )}
-    </div>
+    {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+    <PhotoList/>
+  </div>
   );
 };
 
