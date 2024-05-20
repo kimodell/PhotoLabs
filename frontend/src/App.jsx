2488f1663dc90e28ad6883fefff7cc74c740cd5e
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-20 19:41:31 -0400

    Modify App to display TopicList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,16 @@ import React from 'react';
  // import PhotoListItem from './components/PhotoListItem';
  import PhotoList from './components/PhotoList';
  import './App.scss';
+import TopicList from 'components/TopicList';
+import TopicListItem from 'components/TopicListItem';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-    <PhotoList/>
+    <TopicList />
+    <PhotoList />
   </div>
   );
 };
