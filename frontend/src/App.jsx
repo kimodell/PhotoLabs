e4450ce83cee0f37b3c8e6110caced9329cd75d6
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-21 18:15:22 -0400

    Remove TopicList compenenent from App function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
  // import PhotoListItem from './components/PhotoListItem';
- import PhotoList from './components/PhotoList';
- import './App.scss';
+import PhotoList from './components/PhotoList';
+import './App.scss';
 import TopicList from 'components/TopicList';
-import TopicListItem from 'components/TopicListItem';
+import TopNavigation from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
+    <TopNavigation />  
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-    <TopicList />
     <PhotoList />
   </div>
   );
