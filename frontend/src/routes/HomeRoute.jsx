cc2f955a7f64645dcbcbb3898324da87c9ee749c
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-21 18:43:09 -0400

    Build HomeRoute to view nav bar and photo list on homepage

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import '../styles/HomeRoute.scss';
+import TopNavigation from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
 
+//homepage route to view photos and nav bar from homepage
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation />
+      <PhotoList />
     </div>
   );
 };
