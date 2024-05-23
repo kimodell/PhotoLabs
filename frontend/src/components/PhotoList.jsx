44aa2132aa7d4ec26b72313a6dc5988c6610805c
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-23 19:33:00 -0400

    Pass props from HomeRoute to render and toggle like button

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -6,10 +6,14 @@ import PhotoListItem from "./PhotoListItem";
 //Component to display grid of photos
 const PhotoList = (props) => {
 
+  const { likedPhotos, toggleLike } = props
+
   const parsedPhotos = props.photos.map((photo) => 
     <PhotoListItem 
     key={photo.id} 
-    photo={photo} 
+    photo={photo}
+    liked={likedPhotos.includes(photo.id)} 
+    toggleLike={() => toggleLike(photo.id)} 
     />
     )
 
