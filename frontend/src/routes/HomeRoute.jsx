1318cb20998d34bcdd1889c974669acf05ba4476
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-23 19:29:52 -0400

    Refactor HomeRoute to useState to store favorites globally

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
@@ -6,10 +6,25 @@ import PhotoList from 'components/PhotoList';
 
 //Homepage route to view photos and nav bar from homepage
 const HomeRoute = (props) => {
+
+  //Set inital state of liked photos to empty array
+  const [likedPhotos, setLikedPhotos] = useState([]);
+
+  //Function to check if photoID is already present in likedPhotos state, if not, add the photoID
+  const toggleLike = function(photoId) {
+    if (likedPhotos.includes(photoId)) {
+      const updatedLikedPhotos = likedPhotos.filter(id => id !== photoId);
+      setLikedPhotos(updatedLikedPhotos);
+    } else {
+      const updatedLikedPhotos = [...likedPhotos, photoId];
+      setLikedPhotos(updatedLikedPhotos);
+    }
+  };
+
   return (
     <div className="home-route">
-      <TopNavigation topics={props.topics}/>
-      <PhotoList photos={props.photos}/>
+      <TopNavigation topics={props.topics} />
+      <PhotoList photos={props.photos} likedPhotos={likedPhotos} toggleLike={toggleLike}/>
     </div>
   );
 };
