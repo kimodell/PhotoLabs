61ba18d931dbea21b0e9ac222ff4ec5abcbcc1de
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-23 19:31:47 -0400

    Pass props from HomeRoute to render like button in photo

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
-
+//Component to display a single photo
 const PhotoListItem = (props) => {
 
-  const { photo } = props
+  const { photo, liked, toggleLike } = props
 
   return (
     <li className="photo-list__item" key={photo.id}>
-      <PhotoFavButton />
+      <PhotoFavButton liked={liked} toggleLike={toggleLike}/>
       <img className="photo-list__image" src={photo.urls.regular}></img>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={photo.user.profile}></img>
