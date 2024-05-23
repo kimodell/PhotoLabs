1cdc5b64c4cf926ff07131316441c1ebe828a98c
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-23 19:34:04 -0400

    Refactor like button to pass props from HomeRoute

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,23 +3,15 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
+//Component to display favorite icon, which is empty or filled depending if like is toggled
 function PhotoFavButton(props) {
 
-  //set inital state to unclicked icon
-  const [like, setLike] = useState(false);
-  
-  //set state to the opposite of the previous state
-  const handleClick = () => {
-    setLike(prevLike => !prevLike);
-  };
-
-  // //change icon style if fav state= true
-  // const favIconClass = like ? "photo-list__fav-icon-svg" : "photo-list__fav-icon";
-
+  const { liked, toggleLike } = props;
+ 
   return (
-    <div className={"photo-list__fav-icon"} onClick={handleClick}>
+    <div className={"photo-list__fav-icon"} onClick={toggleLike}>
       <div className={"photo-list__fav-icon-svg"}>
-      <FavIcon selected={like}/>
+      <FavIcon selected={liked}/>
       </div>
     </div>
   );
