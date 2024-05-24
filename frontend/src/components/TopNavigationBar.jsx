000dea1977f06d27230bacfba804e8146211bff7
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-23 20:06:28 -0400

    Update nav bar fav icon to display when a photo is liked

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
-import '../styles/TopNavigationBar.scss'
+import '../styles/TopNavigationBar.scss';
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 
-//Component to display top nav bar
+//Component to display top nav bar with logo, topics and fave icon
 const TopNavigation = (props) => {
+
+  const { likedPhotos } = props;
+
+  const isFavPhotoExist = likedPhotos.length !== 0;
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={props.topics}/>
-      <FavBadge />
+      <TopicList topics={props.topics} />
+      <FavBadge isFavPhotoExist={isFavPhotoExist} />
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;
