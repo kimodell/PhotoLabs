6a79ef32f6e0819850f1b4a24f8ad98f7238d600
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-23 21:12:40 -0400

    Add state to display modal when photo is clicked

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import topics from 'mocks/topics';
 import photos from 'mocks/photos';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
+  //Use sate to open or close modal, display modal if modalOpen is true
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <div className="App">
-    <HomeRoute topics={topics} photos={photos}/>
+    <HomeRoute topics={topics} 
+    photos={photos} 
+    modalOpen={modalOpen} 
+    setModalOpen={setModalOpen}/>
+    {modalOpen && <PhotoDetailsModal/>}
   </div>
   );
 };
