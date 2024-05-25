4a6369f1dc84e4c677539459e7889eb08fdfa727
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-25 13:49:30 -0400

    Refactor custom hook to use Reducer to set state

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,51 +1,55 @@
-import {useState} from 'react'
-
+import { useReducer } from "react";
 
 
 const useApplicationData = () => {
-  const [state, setState] = useState({
+
+  const initialState = {
     modalOpen: false,
     selectPhoto: {},
     likedPhotos: [],
-  })
-
-   //Change state of selected photo for modal based on modal toggle
-  const toggleModal = (selectedPhoto) => {
-    setState(prevState => ({
-      ...prevState,
-      modalOpen: !prevState.modalOpen,
-    
-    }));
-
-    setState(prevState => ({
-      ...prevState,
-      selectPhoto: selectedPhoto,
-    }));
   };
 
-    //Function to check if photoID is already present in likedPhotos state, if not, add the photoID
-  const toggleLike = function(photoId) {
-    if (state.likedPhotos.includes(photoId)) {
-      const copyOfFavorites = [...state.likedPhotos].filter(id => id !== photoId);
-      setState(prevState => ({
-        ...prevState,
-        likedPhotos: copyOfFavorites,
-      })
-      )
-      return
+  //reducer function to manage state
+  const reducer = (state, action) => {
+    switch (action.type) {
+      //Change state of selected photo for modal based on modal toggle
+      case 'TOGGLE_MODAL':
+        return {
+          ...state,
+          modalOpen: !state.modalOpen,
+          selectPhoto: action.payload,
+        };
+
+      // Check if photoID is already present in likedPhotos state, if not, add the photoID 
+      case 'TOGGLE_LIKE':
+        const isLiked = state.likedPhotos.includes(action.payload);
+        return {
+          ...state,
+          likedPhotos: isLiked
+            ? state.likedPhotos.filter(id => id !== action.payload)
+            : [...state.likedPhotos, action.payload],
+        };
+        
+      default:
+        throw new Error('Unexpected action');
     }
-
-    const updatedLikedPhotos = [...state.likedPhotos, photoId];
-    setState(prevState => ({
-      ...prevState,
-      likedPhotos: updatedLikedPhotos,
-    })
-    )
-
   };
 
-  return {state, toggleModal, toggleLike}
-}
+  //hook to use reducer function to manage state
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // use reducer function to toggle modal and set slected photo
+  const toggleModal = (selectedPhoto) => {
+    dispatch({ type: 'TOGGLE_MODAL', payload: selectedPhoto });
+  };
+
+  // Use reducer function to toggle like state
+  const toggleLike = (photoId) => {
+    dispatch({ type: 'TOGGLE_LIKE', payload: photoId });
+  };
+
+  return { state, toggleModal, toggleLike };
+};
 
 
-export default useApplicationData
+export default useApplicationData;
