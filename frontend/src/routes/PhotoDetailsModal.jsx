20d9a49b2255eafd08cd881846a57f3b29b4e004
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-24 22:17:29 -0400

    Update jsx for style

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -14,7 +14,7 @@ const PhotoDetailsModal = (props) => {
   const { urls, user, location, similar_photos } = selectPhoto.photo;
 
   const similarPhotos = Object.values((similar_photos));
-  
+
 
   return (
     <div className="photo-details-modal">
@@ -22,28 +22,37 @@ const PhotoDetailsModal = (props) => {
         <img src={closeSymbol} alt="close symbol" />
       </button>
       <div>
-      <PhotoFavButton toggleFavourite={toggleLike} photoId={selectPhoto.photo.id} favourites={likedPhotos} />
-      <img className="photo-details-modal__image" src={urls.full} ></img>
-      <div className="photo-details-modal__header">
-        <img className="photo-details-modal__photographer-profile" src={user.profile}></img>
-        <div className="photo-details-modal__photographer-profile">
-          <span>{user.name}</span>
-          <div className="photo-details-modal__photographer-location">
-            <span>{location.city}</span>
-            <span>{location.country}</span>
+        <div className="photo-details-modal__fav-icon">
+        <PhotoFavButton toggleFavourite={toggleLike} photoId={selectPhoto.photo.id} favourites={likedPhotos} />
+        </div>
+        <div className="photo-details-modal__image-container">
+        <img className="photo-details-modal__image" src={urls.full} ></img>
+        </div>
+        <div className="photo-details-modal__top-bar">
+          <img className="photo-details-modal__photographer-profile" src={user.profile}></img>
+            <div className="photo-details-modal__photographer-info">
+              <span>{user.name}</span>
+              <div className="photo-details-modal__photographer-location">
+                <span>{location.city}</span>
+                <>, </>
+                <span>{location.country}</span>
+            </div>
           </div>
         </div>
       </div>
-      </div>
+
+      <div className="photo-details-modal__border"></div>
 
       <div className="photo-details-modal__header">
         <h2>Similar Photos</h2>
       </div>
-      <PhotoList 
-      photos={similarPhotos}
-      likedPhotos={likedPhotos}
-      toggleLike={toggleLike}
-      />
+      <div className="photo-details-modal__images">
+        <PhotoList
+          photos={similarPhotos}
+          likedPhotos={likedPhotos}
+          toggleLike={toggleLike}
+        />
+      </div>
     </div>
   );
 };
