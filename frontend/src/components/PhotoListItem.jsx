785a6232c1448a241a05202c67d32655149382b1
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-24 20:46:31 -0400

    Update onClick

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,12 +6,12 @@ import PhotoFavButton from "./PhotoFavButton";
 //Component to display a single photo
 const PhotoListItem = (props) => {
 
-  const { photo, liked, toggleLike, toggleModal } = props;
+  const { photo, liked, toggleLike, toggleModal, selectPhoto } = props;
 
   return (
     <li className="photo-list__item" key={photo.id}>
       <PhotoFavButton liked={liked} toggleLike={toggleLike} />
-      <img className="photo-list__image" src={photo.urls.regular} onClick={() => toggleModal(photo.id, photo.urls.full)}></img>
+      <img className="photo-list__image" src={photo.urls.regular} onClick={() => toggleModal(photo.id)}></img>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={photo.user.profile}></img>
         <div className="photo-list__user-info">
