46d930bcccc49f09948ce883c842dd810ccc444c
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-26 13:55:39 -0400

    update Homeroute element to use state to render photo and topic data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
-import topics from 'mocks/topics';
-import photos from 'mocks/photos';
+// import topics from 'mocks/topics';
+// import photos from 'mocks/photos';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import useApplicationData from 'hooks/useApplicationData';
 // Note: Rendering a single component to build components in isolation
@@ -12,13 +12,14 @@ const App = () => {
 
   return (
     <div className="App">
-      <HomeRoute topics={topics}
-        photos={photos}
+      <HomeRoute topics={state.topicData}
+        photos={state.photoData}
         modalOpen={state.modalOpen}
         toggleModal={toggleModal}
         selectPhoto={state.selectPhoto}
         toggleLike={toggleLike} 
-        likedPhotos={state.likedPhotos}  />
+        likedPhotos={state.likedPhotos} 
+       />
       {state.modalOpen &&
         <PhotoDetailsModal
           modalOpen={state.modalOpen}
